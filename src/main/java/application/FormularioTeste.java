cd9173a4212bddb1d49ae6882f150aaded23a72c
inmetrics <oliveira.bnhc@gmail.com>
2020-05-03 01:08:32 -0300

    Melhorias de código e criação da classe DriverFactory

--- src/main/java/application/FormularioTeste.java
+++ src/main/java/application/FormularioTeste.java
@@ -1,19 +1,16 @@
 package application;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
+import application.core.DriverFactory;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
 import junit.framework.Assert;
 
 public class FormularioTeste {
@@ -21,25 +18,15 @@ public class FormularioTeste {
 	private AndroidDriver<MobileElement> driver;
 
 	@Before
+	//Faz a chamada do Driver Factory para iniciar a sessão
 	public void inicializarAppium() throws MalformedURLException {
-		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-		desiredCapabilities.setCapability("platformName", "Android");
-		desiredCapabilities.setCapability("deviceName", "emulator-5554");
-		desiredCapabilities.setCapability("automationName", "uiautomator2");
-		desiredCapabilities.setCapability(MobileCapabilityType.APP, "/home/inmetrics/eclipse-workspace/CursoAppium/src/main/resources/AppiumCT.apk");
-
-		driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
-		
-		// timeOut padrão
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-		// Selecionando formulário via Xpath
-		driver.findElement(By.xpath("//*[@text='Formulário']")).click();
+		driver = DriverFactory.getDriver();
 	}
 
 	@After
+	//Encerra a sessão
 	public void tearDown(){
-		driver.quit();
+		DriverFactory.killDriver();
 	}
 	
 	@Test
