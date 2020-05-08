0dacb7d0f4b9bbf974cca736187993ff0d10d21d
inmetrics <oliveira.bnhc@gmail.com>
2020-05-08 00:05:15 -0300

    Melhorias de performance

--- src/main/java/application/core/BaseTest.java
+++ src/main/java/application/core/BaseTest.java
@@ -1,11 +1,18 @@
 package application.core;
 
 import org.junit.After;
+import org.junit.AfterClass;
 
 public class BaseTest {
 	
-	@After
+	//Otimização de performance 
+	@AfterClass
+	public static void finalizaClasse() {
+		DriverFactory.getDriver().resetApp();
+	}
+	
 	//Encerra a sessão
+	@After
 	public void tearDown(){
 		DriverFactory.killDriver();
 	}
