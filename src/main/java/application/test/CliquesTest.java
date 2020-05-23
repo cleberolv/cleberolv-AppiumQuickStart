5e87d044b1e250ef55fead7394a0e0582cf286c1
inmetrics <oliveira.bnhc@gmail.com>
2020-05-23 01:28:28 -0300

    Criada nova funcionalidade para clique duplo

--- src/main/java/application/test/CliquesTest.java
+++ src/main/java/application/test/CliquesTest.java
@@ -2,6 +2,7 @@ package application.test;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
 
 import application.core.BaseTest;
 import application.page.CliquesPage;
@@ -12,16 +13,30 @@ public class CliquesTest extends BaseTest {
 	private MenuPage menu = new MenuPage();
 	private CliquesPage page = new CliquesPage();
 	
+	@Before
+	public void menu() {
+		menu.acessarMenuCliques();
+	}
+	
 	@Test
 	public void cliqueLongo() {
-		//Acessar Menu
-		menu.acessarMenuCliques();
-		
 		//Realizar clique longo
 		page.cliqueLongo();
 		
 		//Verficar texto
 		Assert.assertEquals("Clique Longo", page.obterTextoCampo());
 	}
+	
+	@Test
+	public void cliqueDuplo() {
+		//Realizar clique longo
+		page.clicarPorTexto("Clique duplo");
+		page.clicarPorTexto("Clique duplo");
+		
+		//Verficar texto
+		Assert.assertEquals("Duplo Clique", page.obterTextoCampo());
+	}
+	
+	
 	
 }
