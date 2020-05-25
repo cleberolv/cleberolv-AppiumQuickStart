3827a1eab2d77ceaaa0fc4067a79623f38b0bd7b
inmetrics <oliveira.bnhc@gmail.com>
2020-05-25 14:42:37 -0300

    Melhoria para trabalhar com a massa de dados

---
+++ src/main/java/application/page/SB/SBResetPage.java
@@ -0,0 +1,11 @@
+package application.page.SB;
+
+import application.core.BasePage;
+
+public class SBResetPage extends BasePage {
+	
+	public void resetMassa() {
+		clicarPorTexto("RESET");
+	}
+
+}
---
+++ src/main/java/application/test/SBResetTest.java
@@ -0,0 +1,23 @@
+package application.test;
+
+import org.junit.Test;
+
+import application.core.BaseTest;
+import application.page.MenuPage;
+import application.page.SB.SBResetPage;
+import junit.framework.Assert;
+
+public class SBResetTest extends BaseTest {
+
+	private SBResetPage reset = new SBResetPage();
+	private SBTest test = new SBTest();
+	private MenuPage page = new MenuPage();
+
+	@Test
+	public void restaurarMassa() {
+		test.login();
+		reset.resetMassa();
+		Assert.assertTrue(page.checkElementoTexto("Dados resetados com sucesso!"));
+	}
+
+}
